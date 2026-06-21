70706f63929dec13554511b4adc8163daa6c57ea
JATIN <jatinsamad18@gmail.com>
2026-06-21 22:03:51 +0530

    Add String.js with various string operations

---
+++ Practice/Lession-03/String.js
@@ -0,0 +1,51 @@
+'hello'
+
+alert('hello');
+
+'some' + 'text'
+'sometext'
+
+'some'+'more'+'text'
+'somemoretext'
+
+typeof 2
+'number'
+
+typeof 'hello'
+'string'
+
+'hello'+3
+'hello3'
+
+'$'+20.95+7.99
+'$20.957.99'
+
+'$'+(20.95+7.99)
+'$28.939999999999998'
+
+'$'+(2095+799)/100
+'$28.94'
+
+'items ('+(1+1)+'): $'+(2095+799)/100
+'items (2): $28.94'
+
+alert('items ('+(1+1)+'): $'+(2095+799)/100)
+
+"I'm Learning Javascript"
+"I'm Learning Javascript"
+
+'I\'m learning Javascript'
+"I'm learning Javascript"
+
+alert('some\ntext');
+
+`hello`
+'hello'
+  
+`Items (${1+1}): $${(2095+799)/100}`
+'Items (2): $28.94'
+  
+`some
+text`
+
+'some\ntext'
